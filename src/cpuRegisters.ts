6a752a5b0e0f324f69080b8ec36b1dca8d5d116f
r7kamura <r7kamura@gmail.com>
2018-12-01 16:45:08 +0900

    Add accessors to each necessary CPU register bit
    I think TypeScript's const enum is very good for this use and really love this feature.
    
    Note: decimal and reserved flags won't be used in NES CPU.
    This is why I didn't implement accessors for them.

--- src/cpuRegisters.ts
+++ src/cpuRegisters.ts
@@ -1,5 +1,16 @@
 import { Uint8, Uint16 } from './types';
 
+const enum StatusIndex {
+  Carry,
+  Zero,
+  Interrupt,
+  Decimal,
+  Break,
+  Reserved,
+  Overflow,
+  Negative,
+}
+
 export default class CpuRegisters {
   accumulator: Uint8;
   indexX: Uint8;
@@ -15,5 +26,66 @@ export default class CpuRegisters {
     this.programCounter = 0;
     this.stackPointer = 0;
     this.status = 0;
+  }
+
+  get break(): boolean {
+    return this.maskStatusBit(StatusIndex.Break);
+  }
+
+  get carry(): boolean {
+    return this.maskStatusBit(StatusIndex.Carry);
+  }
+
+  get interrupt(): boolean {
+    return this.maskStatusBit(StatusIndex.Interrupt);
+  }
+
+  get negative(): boolean {
+    return this.maskStatusBit(StatusIndex.Negative);
+  }
+
+  get overflow(): boolean {
+    return this.maskStatusBit(StatusIndex.Overflow);
+  }
+
+  get zero(): boolean {
+    return this.maskStatusBit(StatusIndex.Zero);
+  }
+
+  set break(value: boolean) {
+    this.toggleStatusBit(StatusIndex.Break, value);
+  }
+
+  set carry(value: boolean) {
+    this.toggleStatusBit(StatusIndex.Carry, value);
+  }
+
+  set interrupt(value: boolean) {
+    this.toggleStatusBit(StatusIndex.Interrupt, value);
+  }
+
+  set negative(value: boolean) {
+    this.toggleStatusBit(StatusIndex.Negative, value);
+  }
+
+  set overflow(value: boolean) {
+    this.toggleStatusBit(StatusIndex.Overflow, value);
+  }
+
+  set zero(value: boolean) {
+    this.toggleStatusBit(StatusIndex.Zero, value);
+  }
+
+  private maskStatusBit(index: StatusIndex): boolean {
+    return (this.status & (1 << index)) !== 0;
+  }
+
+  private toggleStatusBit(index: StatusIndex, value: boolean) {
+    const maskedByte = (1 << index);
+    if (value) {
+      this.status |= maskedByte
+    } else {
+      this.status &= ~maskedByte
+    }
   }
 }
