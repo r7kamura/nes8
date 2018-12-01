fe5d0b3b7434f49be69f4b39366ce7a759b9b808
r7kamura <r7kamura@gmail.com>
2018-12-01 16:53:58 +0900

    Reset CPU registers before running Emulator

--- src/cpu.ts
+++ src/cpu.ts
@@ -11,4 +11,8 @@ export default class Cpu {
   step(): number {
     return 1;
   }
+
+  reset() {
+    this.cpuRegisters.reset();
+  }
 }
--- src/emulator.ts
+++ src/emulator.ts
@@ -16,6 +16,7 @@ export default class Emulator {
   }
 
   run() {
+    this.cpu.reset();
     while(true) {
       this.step();
     }
