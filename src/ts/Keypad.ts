5aea19dd3f13f154b53a3212b35b1782c84e2062
r7kamura <r7kamura@gmail.com>
2018-12-23 20:54:21 +0900

    Implement keypads

--- src/ts/Keypad.ts
+++ src/ts/Keypad.ts
@@ -1,11 +1,55 @@
 import { Uint8 } from "./types";
 
+interface IKeyMap {
+  [key: string]: number;
+}
+
 export default class Keypad {
-  // @todo
-  public read(): Uint8 {
-    return 0;
+  private buffer: number;
+
+  private copy: number;
+
+  private index: number;
+
+  private keyMap: IKeyMap;
+
+  private latch: boolean;
+
+  constructor(keys: number[]) {
+    this.buffer = 0;
+    this.copy = 0;
+    this.index = 0;
+    this.latch = false;
+    this.keyMap = keys.reduce(
+      (accumulator: IKeyMap, key: number, index: number) => {
+        accumulator[key] = index;
+        return accumulator;
+      },
+      {}
+    );
   }
 
-  // @todo
-  public write(value: Uint8) {}
+  public onKeyDown = (event: KeyboardEvent) => {
+    const index = this.keyMap[event.keyCode];
+    if (index) {
+      this.buffer |= 1 << index;
+    }
+  };
+
+  public read(): Uint8 {
+    const value = this.copy & (1 << this.index) ? 1 : 0;
+    this.index = (this.index + 1) & 0x0f;
+    return value;
+  }
+
+  public write(value: Uint8) {
+    if (value & 1) {
+      this.latch = true;
+    } else {
+      this.latch = false;
+      this.copy = this.buffer;
+      this.buffer = 0;
+      this.index = 0;
+    }
+  }
 }
